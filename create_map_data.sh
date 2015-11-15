52b3745d5a23e011d6c17e8f3805765b98aee530
Michael Suelmann <suelmann@gmx.de>
2015-11-15 23:42:39 +0100

    Anpassung der Pfade für ffms-map

--- create_map_data.sh
+++ create_map_data.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-cd /root/Downloads/ffmap-backend
 for i in /proc/sys/net/ipv4/conf/bat*; do
     num=${i#*bat}
-    mkdir -p data${num}
-    ./backend.py -d data${num} -m bat${num}:/run/alfred${num}.sock 
+    mkdir -p /var/ffmap/data${num}
+    /usr/src/ffmap-backend/backend.py -d /var/ffmap/data${num} -m bat${num}:/run/alfred${num}.sock 
 done
-mkdir -p legacy_data
-wget https://freifunk-muensterland.de/map/data/nodes.json -O legacy_data/nodes.json
-wget https://freifunk-muensterland.de/map/data/graph.json -O legacy_data/graph.json
-./merge_map_data.py -o /var/www/html/data legacy_data data*
+mkdir -p /var/ffmap/legacy_data
+wget https://freifunk-muensterland.de/map/data/nodes.json -O /var/ffmap/legacy_data/nodes.json
+wget https://freifunk-muensterland.de/map/data/graph.json -O /var/ffmap/legacy_data/graph.json
+mkdir -p /var/www/html/data
+/usr/src/tools/merge_map_data.py -o /var/www/html/data /var/ffmap/legacy_data /var/ffmap/data*
