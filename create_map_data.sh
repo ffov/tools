a3275f335aa3fd4789a97328160c858c0396da81
Michael Suelmann <suelmann@gmx.de>
2015-11-17 21:04:27 +0100

    Neue URL für Legacy-Domain

--- create_map_data.sh
+++ create_map_data.sh
@@ -5,7 +5,7 @@ for i in /proc/sys/net/ipv4/conf/bat*; do
     /usr/src/ffmap-backend/backend.py -d /var/ffmap/data${num} -m bat${num}:/run/alfred${num}.sock 
 done
 mkdir -p /var/ffmap/legacy_data
-wget https://freifunk-muensterland.de/map/data/nodes.json -O /var/ffmap/legacy_data/nodes.json
-wget https://freifunk-muensterland.de/map/data/graph.json -O /var/ffmap/legacy_data/graph.json
+wget https://freifunk-muensterland.de/map/data_alt/nodes.json -O /var/ffmap/legacy_data/nodes.json
+wget https://freifunk-muensterland.de/map/data_alt/graph.json -O /var/ffmap/legacy_data/graph.json
 mkdir -p /var/www/html/data
 /usr/src/tools/merge_map_data.py -o /var/www/html/data /var/ffmap/legacy_data /var/ffmap/data*
