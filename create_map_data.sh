4f38390ea224d134b4774c4bcbfab2515ac9a8ce
Michael Suelmann <suelmann@gmx.de>
2015-11-18 00:18:56 +0100

    Download von Warendorf-Domain hinzugefügt

--- create_map_data.sh
+++ create_map_data.sh
@@ -4,6 +4,9 @@ for i in /proc/sys/net/ipv4/conf/bat*; do
     mkdir -p /var/ffmap/data${num}
     /usr/src/ffmap-backend/backend.py -d /var/ffmap/data${num} -m bat${num}:/run/alfred${num}.sock 
 done
+mkdir -p /var/ffmap/data_waf
+wget http://ffwaf-srv2.freifunk-muensterland.net/o/nodes.json -O /var/ffmap/data_waf/nodes.json
+wget http://ffwaf-srv2.freifunk-muensterland.net/o/graph.json -O /var/ffmap/data_waf/graph.json
 mkdir -p /var/ffmap/legacy_data
 wget https://freifunk-muensterland.de/map/data_alt/nodes.json -O /var/ffmap/legacy_data/nodes.json
 wget https://freifunk-muensterland.de/map/data_alt/graph.json -O /var/ffmap/legacy_data/graph.json
