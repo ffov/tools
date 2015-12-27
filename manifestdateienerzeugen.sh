d4b84cc5e1e8d7bb7aac38cc7184c47f2e396f90
Matthias P. Walther <matthiaspeterw@aol.com>
2015-12-27 22:48:45 +0100

    - Skript erstellt, dass die Manifestdateien auch nachträglich erstellen kann

---
+++ manifestdateienerzeugen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PRIORITY=0
+
+cd $1
+datum=`date +"%Y-%m-%d %H:%M:%S%:z"`
+
+for i in stable beta experimental; do
+	echo "BRANCH=$i" > $i.manifest
+	echo "DATE=$datum" >> $i.manifest
+	echo "PRIORITY=$PRIORITY" >> $i.manifest
+	echo >> $i.manifest
+	for j in *bin; do
+		model=${j#*-*-*-*}
+		model=${model%-sysupgrade.bin}
+		version=${j#*-*-}
+		version=${version%-$model-sysupgrade.bin}
+		pruefsumme=`sha512sum $j`
+		echo "$model $version $pruefsumme $j" >> $i.manifest
+	done
+done
+
