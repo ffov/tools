27d8c4d1c973b865d63b110dbf6ea7704fb5ab68
root <suelmann@gmx.de>
2016-01-07 20:50:45 +0100

    - /maps-Prefix für alle Verzeichnisse mit Kartendaten/-Seiten hinzugefügt - Legacy-Domäne anzeigbar gemacht

--- create_map_data.sh
+++ create_map_data.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 for i in /proc/sys/net/ipv4/conf/bat*; do
     num=${i#*bat}
-    mkdir -p /var/www/html/data${num}
-    /usr/src/ffmap-backend/backend.py -d /var/www/html/data${num} -m bat${num}:/run/alfred.$(printf %02d ${num}).sock 
+    mkdir -p /var/www/html/maps/data${num}
+    /usr/src/ffmap-backend/backend.py -d /var/www/html/maps/data${num} -m bat${num}:/run/alfred.$(printf %02d ${num}).sock 
 done
-mkdir -p /var/www/html/data_waf
-wget http://ffwaf-srv2.freifunk-muensterland.net/o/nodes.json -O /var/www/html/data_waf/nodes.json
-wget http://ffwaf-srv2.freifunk-muensterland.net/o/graph.json -O /var/www/html/data_waf/graph.json
-mkdir -p /var/www/html/legacy_data
-wget https://freifunk-muensterland.de/map/data_alt/nodes.json -O /var/www/html/legacy_data/nodes.json
-wget https://freifunk-muensterland.de/map/data_alt/graph.json -O /var/www/html/legacy_data/graph.json
-mkdir -p /var/www/html/data
-/usr/src/tools/merge_map_data.py -o /var/www/html/data /var/www/html/legacy_data /var/www/html/data?*
+mkdir -p /var/www/html/maps/data_waf
+wget http://ffwaf-srv2.freifunk-muensterland.net/o/nodes.json -O /var/www/html/maps/data_waf/nodes.json
+wget http://ffwaf-srv2.freifunk-muensterland.net/o/graph.json -O /var/www/html/maps/data_waf/graph.json
+mkdir -p /var/www/html/maps/data_xlegacy
+wget https://freifunk-muensterland.de/map/data_alt/nodes.json -O /var/www/html/maps/data_xlegacy/nodes.json
+wget https://freifunk-muensterland.de/map/data_alt/graph.json -O /var/www/html/maps/data_xlegacy/graph.json
+mkdir -p /var/www/html/maps/data
+/usr/src/tools/merge_map_data.py -o /var/www/html/maps/data /var/www/html/maps/data?*
 
-for i in /var/www/html/data?*; do
+for i in /var/www/html/maps/data?*; do
     suf=${i#*data}
-    if [ ! -e /var/www/html/map${suf} ]; then
-        mkdir /var/www/html/map${suf} && cd /var/www/html/map${suf} && ln -s ../map/* . && rm config.json && sed -e "s#/data/#/data$suf/#" -e "s#Münsterland#Münsterland - Domäne ${suf}#" <../map/config.json >config.json
-        cat <<EOF > /var/www/html/index.html
+    if [ ! -e /var/www/html/maps/map${suf} ]; then
+        name=$(echo $suf | sed -e s/_waf/Warendorf/ -e s/_xlegacy/Legacy/)
+        mkdir /var/www/html/maps/map${suf} && cd /var/www/html/maps/map${suf} && ln -s ../map/* . && rm config.json && sed -e "s#/data/#/data$suf/#" -e "s#Münsterland#Münsterland - Domäne ${name}#" <../map/config.json >config.json
+        cat <<EOF > /var/www/html/maps/index.html
 <html>
 <head>
 <meta charset="UTF-8">
@@ -27,12 +28,12 @@ for i in /var/www/html/data?*; do
 <h1>Freifunk Münsterland - Karten</h1>
 <p><a href="map/">Gesamtkarte</a></p>
 EOF
-	for j in /var/www/html/data?*; do
+	for j in /var/www/html/maps/data?*; do
 	    suf2=${j#*data}
-	    name=$(echo $suf2 | sed -e s/_waf/Warendorf/)
-	    echo "<p><a href=\"map$suf2/\">Domäne $name</a></p>" >> /var/www/html/index.html
+	    name=$(echo $suf2 | sed -e s/_waf/Warendorf/ -e s/_xlegacy/Legacy/)
+	    echo "<p><a href=\"map$suf2/\">Domäne $name</a></p>" >> /var/www/html/maps/index.html
         done
-        cat <<EOF >> /var/www/html/index.html
+        cat <<EOF >> /var/www/html/maps/index.html
 </body>
 </html>
 EOF
