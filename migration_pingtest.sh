26fce3eb337e4d4e40c98b9fdc5bd9f6e9d3691c
Simon Wüllhorst <mail@simon-wuellhorst.de>
2016-03-26 14:42:40 +0100

    Updated migration_pingtest.sh - Added support for multiple IPv6 Networks - Disable Node in Config, if firmware was loaded once.

--- migration_pingtest.sh
+++ migration_pingtest.sh
@@ -3,7 +3,7 @@
 date
 cat hostlist.txt |  while read output
 do
-    naddr=`echo $output | sed -e "s/:0:/:$2:/g"`
+    naddr=`echo $output | sed -re "s/2a03:2260:115:[0-9a-f]+:/2a03:2260:115:$2:/g"`
     ping6 -c 1 "$naddr" > /dev/null
     if [ $? -eq 0 ]; then
     sed -r -i.bak "/[ ]*$output(.*)/d" $1
@@ -15,7 +15,7 @@ do
     echo "node $output is up" 
     else
     echo "node $output is down"
-    sed -r -i.bak "/([ ]*$output[ ]+1;[ ]*)#(READY )*(.*)/ s//\1#READY \3/" $1
+    sed -r -i.bak "/([ ]*$output[ ]+)1;[ ]*#(READY )*(.*)/ s//\10; #READY \3/" $1
     fi
     fi
 done
