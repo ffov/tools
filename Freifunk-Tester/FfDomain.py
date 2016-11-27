3481f0c70ebd5038f1313dd721ed177ce251a6a9
Matthias Walther <matthiaspeterw@aol.com>
2016-11-27 13:00:17 +0100

    - only one serial instance per domain

--- Freifunk-Tester/FfDomain.py
+++ Freifunk-Tester/FfDomain.py
@@ -7,14 +7,19 @@ SERIAL_TIMEOUT=2
 class FfDomain(libvirt.virDomain):
     def __init__(self, virConnect, vmName):
         self._domain = virConnect.lookupByName(vmName)
+        self._serial = self.__setupSerial()
 
     def __del__(self):
         self._domain.__del__()
+        self._serial.__del__()
         
-    def getSerial(self):
+    def __setupSerial(self):
         domain_xml = self._domain.XMLDesc()
         xml_root = ET.fromstring(domain_xml) 
         devices = xml_root.find('devices')
         console = devices.find('console')
         serialPath = console.get('tty')
         return serial.Serial(serialPath, timeout=SERIAL_TIMEOUT)
+
+    def getSerial(self):
+        return self._serial
