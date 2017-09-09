feaf13743e10d67390513ffc6545ba61db20871a
Matthias Walther <matthiaspeterw@aol.com>
2017-09-09 04:31:30 +0200

    - Passendes Clientnetz automatisch anlegen  - Kleine Erweiterungen

--- set_up_gluon.py
+++ set_up_gluon.py
@@ -12,6 +12,8 @@ DEFAULT_DESTINATION_PATH="/var/lib/libvirt/images"
 LIBVIRT_SYSTEM_PATH='qemu:///system'
 
 name = None
+domain = None
+gluon_domain = None
 libvirt_connection = None
 
 TEMPLATE ="""<domain type='kvm'>
@@ -56,33 +58,23 @@ TEMPLATE ="""<domain type='kvm'>
       <alias name='virtio-disk0'/>
       <address type='pci' domain='0x0000' bus='0x00' slot='0x08' function='0x0'/>
     </disk>
-    <controller type='virtio-serial' index='0'>
-      <alias name='virtio-serial0'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
-    </controller>
     <interface type='network'>
-      <mac address='52:54:00:12:db:a1'/>
-      <source network='Clientnetz-Dom16'/>
-      <target dev='vnet17'/>
+      <source network='$clientnetz'/>
       <model type='virtio'/>
       <alias name='net0'/>
       <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
     </interface>
     <interface type='network'>
-      <mac address='52:54:00:be:09:ba'/>
       <source network='default'/>
-      <target dev='vnet18'/>
       <model type='virtio'/>
       <alias name='net1'/>
       <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
     </interface>
     <serial type='pty'>
-      <source path='/dev/pts/14'/>
       <target port='0'/>
       <alias name='serial0'/>
     </serial>
-    <console type='pty' tty='/dev/pts/14'>
-      <source path='/dev/pts/14'/>
+    <console type='pty' >
       <target type='serial' port='0'/>
       <alias name='serial0'/>
     </console>
@@ -111,6 +103,26 @@ TEMPLATE ="""<domain type='kvm'>
     <redirdev bus='usb' type='spicevmc'>
       <alias name='redir1'/>
     </redirdev>
+     <controller type='usb' index='0' model='ich9-ehci1'>
+       <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x7'/>
+     </controller>
+     <controller type='usb' index='0' model='ich9-uhci1'>
+       <master startport='0'/>
+       <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0' multifunction='on'/>
+     </controller>
+     <controller type='usb' index='0' model='ich9-uhci2'>
+       <master startport='2'/>
+       <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x1'/>
+     </controller>
+     <controller type='usb' index='0' model='ich9-uhci3'>
+       <master startport='4'/>
+       <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x2'/>
+     </controller>
+     <controller type='pci' index='0' model='pci-root'/>
+    <controller type='virtio-serial' index='0'>
+      <alias name='virtio-serial0'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
+    </controller>
     <memballoon model='virtio'>
       <alias name='balloon0'/>
       <address type='pci' domain='0x0000' bus='0x00' slot='0x09' function='0x0'/>
@@ -119,12 +131,11 @@ TEMPLATE ="""<domain type='kvm'>
 </domain>"""
 
 TEMPLATE_NETZ="""<network connections='1'>
-  <name>$netName</name>
+  <name>$clientnetz</name>
   <bridge stp='on' delay='0'/>
-  <domain name='$netName'/>
 </network>"""
 
-def download_image_file(link):
+def download_image_file_and_unzip_it(link):
    global name
    splits = sys.argv[1].split('/')
    n = len(splits)
@@ -138,19 +149,49 @@ def download_image_file(link):
    outF.close()
    os.remove(name)
 
+def open_libvirt_connection():
+    global libvirt_connection
+    libvirt_connection = libvirt.open(LIBVIRT_SYSTEM_PATH) 
+
 def install_vm():
     global TEMPLATE
-    global name
+    global name, domain, gluon_domain
     global libvirt_connection
-    xml = TEMPLATE.replace('$vmName', 'Test').replace('$imagePath', DEFAULT_DESTINATION_PATH).replace('$vmIMG', name.replace(".gz",""))
-    libvirt_connection = libvirt.open(LIBVIRT_SYSTEM_PATH) 
-    libvirt_connection.createXML(xml)
+    xml = TEMPLATE.replace('$vmName', domain).replace('$imagePath', DEFAULT_DESTINATION_PATH).replace('$vmIMG', name.replace(".gz","")).replace('$clientnetz', 'Clientnetz-'+domain)
+    gluon_domain = libvirt_connection.defineXML(xml)
+    gluon_domain.create()
 
-   
+def extract_domain():
+    global name, domain   
+    domain = name.split("-")[1]
+    print(domain)
+
+def setup_clientnetz():
+    global domain
+    global TEMPLATE_NETZ
+    global libvirt_connection
+    net_xml = TEMPLATE_NETZ.replace('$clientnetz', 'Clientnetz-'+domain)
+    try:
+        net = libvirt_connection.networkDefineXML(net_xml)
+        net.setAutostart(1)
+        net.create()
+    except:
+        print('Network exists already')
+
+def __getSerialPath():
+    global gluon_domain
+    domain_xml = gluon_domain.XMLDesc()
+    xml_root = ET.fromstring(domain_xml)
+    devices = xml_root.find('devices')
+    console = devices.find('console')
+    serialPath = console.get('tty')
+    return serialPath
 
 
 
-
-download_image_file(sys.argv[1])
+download_image_file_and_unzip_it(sys.argv[1])
+open_libvirt_connection()
+extract_domain()
+setup_clientnetz()
 install_vm()
-   
+
