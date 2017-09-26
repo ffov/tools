21bbc5a268cc88a0dbb70c8ba72bc437975e416b
Matthias Walther <matthiaspeterw@aol.com>
2017-09-27 00:12:45 +0200

    - Standardpfad angepasst

--- Freifunk-Tester/icinga_reporter.sh
+++ Freifunk-Tester/icinga_reporter.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 MAX_AGE=10800
-DATA_PATH='.'
+DATA_PATH='/root/testresults'
+FILENAME="$DATA_PATH/$1_$2_$3"
 
-tail -n-1 $1_$2_$3
-time=$(date -r $1_$2_$3 +%s)
+
+tail -n-1 $FILENAME
+time=$(date -r $FILENAME +%s)
 now=$(date +%s)
 diff=$(( $time - $now ))
 if [[ $diff -gt $MAX_AGE ]]
 then
 	exit 3
 else
-	exit $(head -n1 $1_$2_$3)
+	exit $(head -n1 $FILENAME)
 fi
 
